cf8b49faca0dd9076736f5c396e9479ca1721385
Dang Vu Nhat Quang <ghitacluber@gmail.com>
2020-09-26 17:06:00 +0700

    second commit

---
+++ app/controllers/api/welcome_controller.rb
@@ -0,0 +1,10 @@
+class Api::WelcomeController < ApiController
+  def index
+    render json: {success: true, message: "Hello World"}
+  end
+
+  def show
+    @post = Post.all
+    render json: {success: true, post: @post, message: "Here we go again !!"}
+  end
+end
